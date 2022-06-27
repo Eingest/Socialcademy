d80563e24bd2bc1d548354f3abfed6446ff57f61
Eingest <ak0805@live.de>
2022-06-27 20:00:17 +0200

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Andreas Kiesel on 27.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
