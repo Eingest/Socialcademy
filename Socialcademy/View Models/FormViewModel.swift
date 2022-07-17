c38bb5e120f9b0c8819b9ed8d0f56005883abb6e
Eingest <ak0805@live.de>
2022-07-17 19:19:50 +0200

    authentication part 1

---
+++ Socialcademy/View Models/FormViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  FormViewModel.swift
+//  Socialcademy
+//
+//  Created by Andreas Kiesel on 17.07.22.
+//
+
+import Foundation
+
+@MainActor
+@dynamicMemberLookup
+class FormViewModel<Value>: ObservableObject {
+    typealias Action = (Value) async throws -> Void
+    
+    @Published var value: Value
+    @Published var error: Error?
+    
+    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
+        get { value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
+    }
+    
+    private let action: Action
+    
+    init(initialValue: Value, action: @escaping Action) {
+        self.value = initialValue
+        self.action = action
+    }
+    
+    private func handleSubmit() async {
+        do {
+            try await action(value)
+        } catch {
+            print("[FormViewModel] Cannot submit: \(error)")
+            self.error = error
+        }
+    }
+    
+    nonisolated func submit() {
+        Task {
+            await handleSubmit()
+        }
+    }
+}
---
+++ Socialcademy/Views/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Socialcademy
+//
+//  Created by Andreas Kiesel on 17.07.22.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct ProfileView: View {
+    var body: some View {
+        Button("Sign Out", action: {
+            try! Auth.auth().signOut()
+        })
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
